6ce9cd7d42b8a6eba66a087949e5b920ec4f395c
Bassem Sadaqah <bassem.sadaqah@gmail.com>
2022-03-03 12:50:13 +0200

    FLC

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ var app = express();
 app.use(express.static('public'));
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, function () {
+    console.log('Mailer started')
+})
